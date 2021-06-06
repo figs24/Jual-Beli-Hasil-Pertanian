d488ba677338d3f34277db7a52207ef5293a6f3c
figs24 <pwsvnp@gmail.com>
2021-06-06 18:11:27 +0800

    DAO
    eror lane 97

--- Pesanan/src/DAO/DAOBrg.java
+++ Pesanan/src/DAO/DAOBrg.java
@@ -10,7 +10,10 @@ import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -73,11 +76,8 @@ public class DAOBrg implements ImpBrg{
                 statement.setString(1,mb.getNamaBrg());
                 statement.setInt(2,mb.getJumlah());
                 statement.setDouble(3,mb.getHarga());
+                statement.setDouble(4,mb.getKode());
                 statement.executeUpdate();
-                ResultSet rs=statement.getGeneratedKeys();
-                while (rs.next()){
-                    mb.setKode(rs.getInt(1));
-                }
             }catch (SQLException ex){
                 ex.printStackTrace();
             }finally{
@@ -91,11 +91,37 @@ public class DAOBrg implements ImpBrg{
 
     @Override
     public List<MdBrg> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<MdBrg> lmb=null;
+        try {
+            lmb=new ArrayList<MdBrg>();
+            Statement st=connection.createStatement();
+            
+            ResultSet rs=st.executedQuery(select);
+            while(rs.next()){
+                MdBrg mb=new MdBrg();
+                mb.setKode(rs.getInt("kode"));
+                mb.setNamaBrg(rs.getString("namaBrg"));
+                mb.setJumlah(rs.getInt("jumlah"));
+                mb.setHarga(rs.getDouble("harga"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return lmb;
     }
 
     @Override
     public List<MdBrg> getCari(String namaBrg) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static class Statement {
+
+        public Statement() {
+        }
+
+        private ResultSet executedQuery(String select) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
     }
 }
